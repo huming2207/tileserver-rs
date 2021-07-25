e23bbddfc12890fb16bd8a458c170edf52a35f25
Jackson Ming Hu <huming2207@gmail.com>
2021-07-25 17:05:56 +1000

    Use mimalloc

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,16 @@ extern crate lazy_static;
 
 mod model;
 mod util;
+mod api;
+
+use mimalloc::MiMalloc;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
 
 fn main() {
     dotenv::dotenv().ok();
+    env_logger::init();
+
     println!("Hello, world!");
 }
